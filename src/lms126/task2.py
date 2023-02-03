57cf7cbd1c1a47c1234f6d5bf8c49479b8c166d4
NDatAnalyst <neildass@hotmail.com>
2023-02-03 17:19:55 +0100

    Update task2.py

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -1,10 +1,9 @@
 def task2():
-    height = input("height of the rectangle")
-    base = input("base of the rectangle")
-    area = height * base
-    print(area)
-    return
-    '''
+    # height = input("height of the rectangle")
+    # base = input("base of the rectangle")
+    # area = height * base
+    # print(area)
+    return '''
     INPUT height of the rectangle 
     INPUT base of the rectangle
     Calculate the area of the rectangle
