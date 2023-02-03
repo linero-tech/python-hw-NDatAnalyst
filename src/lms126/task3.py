6315c2fb653ff7ff4ba5a9e9b6e2385112a86593
NDatAnalyst <neildass@hotmail.com>
2023-02-03 18:06:35 +0100

    Update task3.py

--- src/lms126/task3.py
+++ src/lms126/task3.py
@@ -2,12 +2,11 @@ from to_do import TODO
 
 
 def task3():
-    height = input("height of the triangle")
-    base = input("base of the triangle")
-    area = (height * base) / 2
-    print(area)
-    return
-    '''
+    # height = input("height of the triangle")
+    # base = input("base of the triangle")
+    # area = height * base / 2
+    # print(area)
+    return '''
     INPUT height of the triangle 
     INPUT base of the triangle
     Calculate the area of the triangle 
