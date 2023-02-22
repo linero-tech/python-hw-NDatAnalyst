64728c46184c1ca0f246af3d77d22518d8bdd2ca
NDatAnalyst <neildass@hotmail.com>
2023-02-22 17:57:07 +0100

    Update to Task 9

--- src/lms127/task9.py
+++ src/lms127/task9.py
@@ -2,6 +2,11 @@ from to_do import TODO
 
 
 def task9(sentence, character):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return character.lower() in sentence.lower()
+
+if __name__ == "__main__":
+    print(task9(sentence="I code in KOTLIN", character='i')) #true
+    print(task9(sentence='I code in KOTLIN', character='I'))#true
+    print(task9(sentence='I code in KOTLIN', character='k')) #true
+    print(task9(sentence='I code in KOTLIN', character='K')) #true
+    print(task9(sentence='I code in KOTLIN', character='z')) #false
