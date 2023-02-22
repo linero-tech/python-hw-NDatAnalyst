a80c14313a87640e4b879d443a7ed26434783bba
NDatAnalyst <neildass@hotmail.com>
2023-02-22 13:22:26 +0100

    Task 5 complete

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -1,18 +1,17 @@
 from to_do import TODO
 
-
 def task3(radius):
-    '''do not overide the radius by creating
-    another one eg, radius = 2. You
-     can also test h/w against the eg
-     given by Humberto in this case 2
-     and the result was 12.57'''
+    '''do not override the radius by creating
+      another one eg, radius = 2. You
+       can also test h/w against the e.g.
+       given by Humberto in this case 2
+       and the result was 12.57'''
     result = 2.0 * 3.1416 * radius
     '''You must also always keep the 
     'return' word as shown below'''
     return result
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     result = task3(radius=2.0)
     print(result)
 
@@ -23,5 +22,3 @@ if __name__ =="__main__":
      print(task3(radius=5.0)) this
      will also give you results for
      each of the print lines'''
-
-
--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -2,9 +2,13 @@ from to_do import TODO
 
 
 def task5(value_for_a, value_for_b):
-    a = value_for_a
-    b = value_for_b
+    a = value_for_b
+    b = value_for_a
 
 
     # Do not erase or change the below statement
     return a, b
+
+if __name__ == "__main__":
+    a, b = task5(value_for_a=1, value_for_b=2)
+    print(a, b)
