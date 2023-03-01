2944d0072a599183f8698a06ea11a69aa4639f68
NDatAnalyst <neildass@hotmail.com>
2023-03-01 15:01:05 +0100

    Update task 5

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -4,13 +4,13 @@ from to_do import TODO
 def task5(value_for_a, value_for_b):
     a = value_for_b
     b = value_for_a
+
+    return a, b
 ''' temp = a
     a = b
     b = temp'''
 
     # Do not erase or change the below statement
-    return a, b
-
 if __name__ == "__main__":
-    a, b = task5(value_for_a=1, value_for_b=2)
+    a, b = task5(value_for_a=5, value_for_b=10)
     print(a, b)
