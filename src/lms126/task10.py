351ab69165e7371a1ed230385138628be792de85
NDatAnalyst <neildass@hotmail.com>
2023-03-07 11:34:47 +0100

    1st update task-10

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -6,7 +6,7 @@ def task10():
     INPUT: New customer email address and password longer than six characters.
     OUTPUT: Verify email is valid, if not return error stating why and offering 
     opportunity to amend. 
-    else if email is valid then calculate that the password has 6 or more characters.
+    elif email is valid then calculate that the password has 6 or more characters.
     Output: create new customer account,
     Else if password has 5 or less characters
-     output return error stating why and offering opportunity to amend.'''
+    output: return error stating why and offering opportunity to amend.'''
--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -2,6 +2,24 @@ from to_do import TODO
 
 
 def task10(password):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    import re
+
+    while True:
+        password = input('enter a new password: ')
+        result = password
+        if (len(password) < 6 or len(password) > 10):
+            print('Your password should be between 6-10 characters.')
+        elif not re.search("[A-Z]", password):
+            print('Your password should include at least one capital letter.')
+        elif not re.search('[a-z]', password):
+            print('Your password should include at least one lower case letter.')
+        elif not re.search('[0-9]', password):
+            print('Your password should include at least one number.')
+        elif not re.search('[$#@]', password):
+            print('Your password should include at least one of these signs: $#@')
+        else:
+            print('Your password is valid.')
+        return result
+
+if __name__ == "__main__":
+    print(task10(password=''))
