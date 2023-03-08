224aa57a66f61545dcc2ce268fac1d867fa1f197
NDatAnalyst <neildass@hotmail.com>
2023-03-08 16:00:37 +0100

    1st update of task2

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -2,6 +2,21 @@ from to_do import TODO
 
 
 def task2(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = number
+    if number > 1:
+        # loop from 2 to n / 2
+        for i in range(2, int(number/2)+1):
+            #If number is divisible by any number
+            #between 2 and n / 2 not prime'''
+            if (number % i) == 0:
+                print(False)
+            else:
+                print(True)
+    else:
+        print(False)
+    return result
+
+if __name__ == "__main__":
+    print(task2(27))
+
+
